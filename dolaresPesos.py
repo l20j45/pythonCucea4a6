17d2e398054a8492f16dd6421bf5dabaec625cc4
daniel <l20j45_@hotmai.com>
2024-02-20 16:08:26 -0600

    clase del jueves

---
+++ dolaresPesos.py
@@ -0,0 +1,6 @@
+DolaresAconvertir = float(input('cuantos dolares vas a convertir: '))
+valorDelDolarAPeso = 17.12
+print("Hola buenas tardes")
+print("total: ")
+
+print(f"Total: {DolaresAconvertir * valorDelDolarAPeso}" )
---
+++ if1.py
@@ -0,0 +1,12 @@
+esEstudiante = "si"
+edad = 16
+dinero= 400
+if (esEstudiante == "si" and edad >=18) :
+    print("puedes entrar")
+    print("No tomes mucho (Guiño)")
+elif dinero >= 500 :
+    print("puedes entrar pero callito")
+    print("no acoses mucho")
+else :
+    print("No hay fiesta")
+print("Hola")
